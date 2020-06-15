2851b18316dae495c1431535ed32c6b5db107c66
Katie Frey <katieefrey@gmail.com>
2020-06-15 12:23:10 -0400

    more updates

--- main/metasat/admin.py
+++ main/metasat/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Element, SchemaType, Segment
+from .models import Element, ElementFamily, Segment
 
 # Register your models here.
 admin.site.register(Element)
-admin.site.register(SchemaType)
+admin.site.register(ElementFamily)
 admin.site.register(Segment)
 
