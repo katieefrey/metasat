dfc3942e12a780b384c12590dc3cd811e1c99905
Allie <allie.williams@cfa.harvard.edu>
2020-06-29 15:06:25 -0400

    settings debug

--- main/main/settings.py
+++ main/main/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 #local
